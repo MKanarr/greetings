51a7c43fbb06bd4298152ca53337d09c6eb3374d
mkanarr <mkanarr@tamu.edu>
2022-04-24 10:36:34 -0500

    removed popover

--- index.js
+++ index.js
@@ -42,9 +42,3 @@ function clipboardCopy(item) {
         alert(e);
     }
 }
-
-
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-});
